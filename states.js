a8a904750861584d5e9bf57cedb9d0065cca8c5a
dinglydo <amrojjeh@gmail.com>
2021-10-23 13:17:25 -0500

    Added basic data

---
+++ states.js
@@ -0,0 +1,202 @@
+let states = {
+	"MA": {
+		"Name": "Massachusetts",
+		"Capital": "Boston",
+	},
+	"MN": {
+		"Name": "Minnesota",
+		"Capital": "Saint Paul",
+	},
+	"MT": {
+		"Name": "Montana",
+		"Capital": "Helena",
+	},
+	"ND": {
+		"Name": "North Dakota",
+		"Capital": "Bismarck"
+	},
+	"HI": {
+		"Name": "Hawaii",
+		"Capital": "Honolulu"
+	},
+	"ID": {
+		"Name": "Idaho",
+		"Capital": "Boise"
+	},
+	"WA": {
+		"Name": "Washington",
+		"Capital": "Olympia"
+	},
+	"AZ": {
+		"Name": "Arizona",
+		"Capital": "Phoenix",
+	},
+	"CA": {
+		"Name": "California",
+		"Capital": "Sacramento"
+	},
+	"CO": {
+		"Name": "Colorado",
+		"Capital": "Denver"
+	},
+	"NV": {
+		"Name": "Nevada",
+		"Capital": "Carson City"
+	},
+	"NM": {
+		"Name": "New Mexico",
+		"Capital": "Santa Fe",
+	},
+	"OR": {
+		"Name": "Oregon",
+		"Capital": "Salem",
+	},
+	"UT": {
+		"Name": "Utah",
+		"Capital": "Salt Lake City",
+	},
+	"WY": {
+		"Name": "Wyoming",
+		"Capital": "Cheyenne",
+	},
+	"AR": {
+		"Name": "Arkansas",
+		"Capital": "Little Rock"
+	},
+	"IA": {
+		"Name": "Iowa",
+		"Capital": "Des Moines",
+	},
+	"KS": {
+		"Name": "Kansas",
+		"Capital": "Topeka",
+	},
+	"MO": {
+		"Name": "Missouri",
+		"Capital": "Jefferson City",
+	},
+	"NE": {
+		"Name": "Nebraska",
+		"Capital": "Lincoln",
+	},
+	"OK": {
+		"Name": "Oklahoma",
+		"Capital": "Oklahoma City"
+	},
+	"SD": {
+		"Name": "South Dakota",
+		"Capital": "Pierre"
+	},
+	"LA": {
+		"Name": "Louisiana",
+		"Capital": "Baton Rouge",
+	},
+	"TX": {
+		"Name": "Texas",
+		"Capital": "Austin"
+	},
+	"CT": {
+		"Name": "Connecticut",
+		"Capital": "Hartford",
+	},
+	"NH": {
+		"Name": "New Hampshire",
+		"Capital": "Concord"
+	},
+	"RI": {
+		"Name": "Rhode Island",
+		"Capital": "Providence",
+	},
+	"VT": {
+		"Name": "Vermont",
+		"Capital": "Montpelier",
+	},
+	"AL": {
+		"Name": "Alabama",
+		"Capital": "Montgomery"
+	},
+	"FL": {
+		"Name": "Florida",
+		"Capital": "Tallahassee"
+	},
+	"GA": {
+		"Name": "Georgia",
+		"Capital": "Atlanta",
+	},
+	"MS": {
+		"Name": "Mississippi",
+		"Capital": "Jackson"
+	},
+	"SC": {
+		"Name": "South Carolina",
+		"Capital": "Columbia",
+	},
+	"IL": {
+		"Name": "Illinois",
+		"Capital": "Springfield",
+	},
+	"IN": {
+		"Name": "Indiana",
+		"Capital": "Indianapolis",
+	},
+	"KY": {
+		"Name": "Kentucky",
+		"Capital": "Fankfort",
+	},
+	"NC": {
+		"Name": "North Carolina",
+		"Capital": "Raleigh",
+	},
+	"OH": {
+		"Name": "Ohio",
+		"Capital": "Columbus",
+	},
+	"TN": {
+		"Name": "Tennessee",
+		"Capital": "Nashvile"
+	},
+	"VA": {
+		"Name": "Virgina",
+		"Capital": "Richmond"
+	},
+	"WI": {
+		"Name": "Wisconsin",
+		"Capital": "Madison"
+	},
+	"WV": {
+		"Name": "West Virgina",
+		"Capital": "Charleston"
+	},
+	"DE": {
+		"Name": "Delaware",
+		"Capital": "Dover",
+	},
+	"MD": {
+		"Name": "Maryland",
+		"Capital": "Annapolis",
+	},
+	"NJ": {
+		"Name": "New Jersey",
+		"Capital": "Trenton",
+	},
+	"NY": {
+		"Name": "New York",
+		"Capital": "Albany",
+	},
+	"PA": {
+		"Name": "Pennsylvania",
+		"Capital": "Harrisburg"
+	},
+	"ME": {
+		"Name": "Maine",
+		"Capital": "Augusta"
+	},
+	"MI": {
+		"Name": "Michigan",
+		"Capital": "Lancing",
+	},
+	"AK": {
+		"Name": "Alaska",
+		"Capital": "Juneau"
+	},
+};
