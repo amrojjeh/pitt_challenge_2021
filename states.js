34810a6b25acf142950a82207659a50ae2eded13
dinglydo <amrojjeh@gmail.com>
2021-10-23 18:05:15 -0500

    update 2

--- states.js
+++ states.js
@@ -1,202 +1,423 @@
 let states = {
-	"MA": {
-		"Name": "Massachusetts",
-		"Capital": "Boston",
-	},
-	"MN": {
-		"Name": "Minnesota",
-		"Capital": "Saint Paul",
-	},
-	"MT": {
-		"Name": "Montana",
-		"Capital": "Helena",
-	},
-	"ND": {
-		"Name": "North Dakota",
-		"Capital": "Bismarck"
-	},
-	"HI": {
-		"Name": "Hawaii",
-		"Capital": "Honolulu"
-	},
-	"ID": {
-		"Name": "Idaho",
-		"Capital": "Boise"
-	},
-	"WA": {
-		"Name": "Washington",
-		"Capital": "Olympia"
-	},
-	"AZ": {
-		"Name": "Arizona",
-		"Capital": "Phoenix",
-	},
-	"CA": {
-		"Name": "California",
-		"Capital": "Sacramento"
-	},
-	"CO": {
-		"Name": "Colorado",
-		"Capital": "Denver"
-	},
-	"NV": {
-		"Name": "Nevada",
-		"Capital": "Carson City"
-	},
-	"NM": {
-		"Name": "New Mexico",
-		"Capital": "Santa Fe",
-	},
-	"OR": {
-		"Name": "Oregon",
-		"Capital": "Salem",
-	},
-	"UT": {
-		"Name": "Utah",
-		"Capital": "Salt Lake City",
-	},
-	"WY": {
-		"Name": "Wyoming",
-		"Capital": "Cheyenne",
-	},
-	"AR": {
-		"Name": "Arkansas",
-		"Capital": "Little Rock"
-	},
-	"IA": {
-		"Name": "Iowa",
-		"Capital": "Des Moines",
-	},
-	"KS": {
-		"Name": "Kansas",
-		"Capital": "Topeka",
-	},
-	"MO": {
-		"Name": "Missouri",
-		"Capital": "Jefferson City",
-	},
-	"NE": {
-		"Name": "Nebraska",
-		"Capital": "Lincoln",
-	},
-	"OK": {
-		"Name": "Oklahoma",
-		"Capital": "Oklahoma City"
-	},
-	"SD": {
-		"Name": "South Dakota",
-		"Capital": "Pierre"
-	},
-	"LA": {
-		"Name": "Louisiana",
-		"Capital": "Baton Rouge",
-	},
-	"TX": {
-		"Name": "Texas",
-		"Capital": "Austin"
-	},
-	"CT": {
-		"Name": "Connecticut",
-		"Capital": "Hartford",
-	},
-	"NH": {
-		"Name": "New Hampshire",
-		"Capital": "Concord"
-	},
-	"RI": {
-		"Name": "Rhode Island",
-		"Capital": "Providence",
-	},
-	"VT": {
-		"Name": "Vermont",
-		"Capital": "Montpelier",
-	},
-	"AL": {
-		"Name": "Alabama",
-		"Capital": "Montgomery"
-	},
-	"FL": {
-		"Name": "Florida",
-		"Capital": "Tallahassee"
-	},
-	"GA": {
-		"Name": "Georgia",
-		"Capital": "Atlanta",
-	},
-	"MS": {
-		"Name": "Mississippi",
-		"Capital": "Jackson"
-	},
-	"SC": {
-		"Name": "South Carolina",
-		"Capital": "Columbia",
-	},
-	"IL": {
-		"Name": "Illinois",
-		"Capital": "Springfield",
-	},
-	"IN": {
-		"Name": "Indiana",
-		"Capital": "Indianapolis",
-	},
-	"KY": {
-		"Name": "Kentucky",
-		"Capital": "Fankfort",
-	},
-	"NC": {
-		"Name": "North Carolina",
-		"Capital": "Raleigh",
-	},
-	"OH": {
-		"Name": "Ohio",
-		"Capital": "Columbus",
-	},
-	"TN": {
-		"Name": "Tennessee",
-		"Capital": "Nashvile"
-	},
-	"VA": {
-		"Name": "Virgina",
-		"Capital": "Richmond"
-	},
-	"WI": {
-		"Name": "Wisconsin",
-		"Capital": "Madison"
-	},
-	"WV": {
-		"Name": "West Virgina",
-		"Capital": "Charleston"
-	},
-	"DE": {
-		"Name": "Delaware",
-		"Capital": "Dover",
-	},
-	"MD": {
-		"Name": "Maryland",
-		"Capital": "Annapolis",
-	},
-	"NJ": {
-		"Name": "New Jersey",
-		"Capital": "Trenton",
-	},
-	"NY": {
-		"Name": "New York",
-		"Capital": "Albany",
-	},
-	"PA": {
-		"Name": "Pennsylvania",
-		"Capital": "Harrisburg"
-	},
-	"ME": {
-		"Name": "Maine",
-		"Capital": "Augusta"
-	},
-	"MI": {
-		"Name": "Michigan",
-		"Capital": "Lancing",
-	},
-	"AK": {
-		"Name": "Alaska",
-		"Capital": "Juneau"
-	},
+  "MA": {
+    "Name": "Massachusetts",
+    "Capital": "Boston",
+    "hidden": {
+      "household_proportion": 11.375426443170607,
+      "tag": {}
+    }
+  },
+  "MN": {
+    "Name": "Minnesota",
+    "Capital": "Saint Paul",
+    "hidden": {
+      "household_proportion": 9.49845068035341,
+      "tag": {}
+    }
+  },
+  "MT": {
+    "Name": "Montana",
+    "Capital": "Helena",
+    "hidden": {
+      "household_proportion": 1.8594921360619903,
+      "tag": {}
+    }
+  },
+  "ND": {
+    "Name": "North Dakota",
+    "Capital": "Bismarck",
+    "hidden": {
+      "household_proportion": 1.3834012020808253,
+      "tag": {}
+    }
+  },
+  "HI": {
+    "Name": "Hawaii",
+    "Capital": "Honolulu",
+    "hidden": {
+      "household_proportion": 1.9966188760587742,
+      "tag": {}
+    }
+  },
+  "ID": {
+    "Name": "Idaho",
+    "Capital": "Boise",
+    "hidden": {
+      "household_proportion": 2.737962894555,
+      "tag": {}
+    }
+  },
+  "WA": {
+    "Name": "Washington",
+    "Capital": "Olympia",
+    "hidden": {
+      "household_proportion": 12.378894485465079,
+      "tag": {}
+    }
+  },
+  "AZ": {
+    "Name": "Arizona",
+    "Capital": "Phoenix",
+    "hidden": {
+      "household_proportion": 11.174519015562732,
+      "tag": {}
+    }
+  },
+  "CA": {
+    "Name": "California",
+    "Capital": "Sacramento",
+    "hidden": {
+      "household_proportion": 56.689306000408514,
+      "tag": {}
+    }
+  },
+  "CO": {
+    "Name": "Colorado",
+    "Capital": "Denver",
+    "hidden": {
+      "household_proportion": 9.33935098065632,
+      "tag": {}
+    }
+  },
+  "NV": {
+    "Name": "Nevada",
+    "Capital": "Carson City",
+    "hidden": {
+      "household_proportion": 4.774433835576551,
+      "tag": {}
+    }
+  },
+  "NM": {
+    "Name": "New Mexico",
+    "Capital": "Santa Fe",
+    "hidden": {
+      "household_proportion": 3.390897910048196,
+      "tag": {}
+    }
+  },
+  "OR": {
+    "Name": "Oregon",
+    "Capital": "Salem",
+    "hidden": {
+      "household_proportion": 7.005541045019361,
+      "tag": {}
+    }
+  },
+  "UT": {
+    "Name": "Utah",
+    "Capital": "Salt Lake City",
+    "hidden": {
+      "household_proportion": 4.247321828240642,
+      "tag": {}
+    }
+  },
+  "WY": {
+    "Name": "Wyoming",
+    "Capital": "Cheyenne",
+    "hidden": {
+      "household_proportion": 1,
+      "tag": {}
+    }
+  },
+  "AR": {
+    "Name": "Arkansas",
+    "Capital": "Little Rock",
+    "hidden": {
+      "household_proportion": 5.032881213032538,
+      "tag": {}
+    }
+  },
+  "IA": {
+    "Name": "Iowa",
+    "Capital": "Des Moines",
+    "hidden": {
+      "household_proportion": 5.499641461792865,
+      "tag": {}
+    }
+  },
+  "KS": {
+    "Name": "Kansas",
+    "Capital": "Topeka",
+    "hidden": {
+      "household_proportion": 4.9075275639827725,
+      "tag": {}
+    }
+  },
+  "MO": {
+    "Name": "Missouri",
+    "Capital": "Jefferson City",
+    "hidden": {
+      "household_proportion": 10.493309459758976,
+      "tag": {}
+    }
+  },
+  "NE": {
+    "Name": "Nebraska",
+    "Capital": "Lincoln",
+    "hidden": {
+      "household_proportion": 3.299316387151729,
+      "tag": {}
+    }
+  },
+  "OK": {
+    "Name": "Oklahoma",
+    "Capital": "Oklahoma City",
+    "hidden": {
+      "household_proportion": 6.432223241098474,
+      "tag": {}
+    }
+  },
+  "SD": {
+    "Name": "South Dakota",
+    "Capital": "Pierre",
+    "hidden": {
+      "household_proportion": 1.4967210051238369,
+      "tag": {}
+    }
+  },
+  "LA": {
+    "Name": "Louisiana",
+    "Capital": "Baton Rouge",
+    "hidden": {
+      "household_proportion": 7.559710735720401,
+      "tag": {}
+    }
+  },
+  "TX": {
+    "Name": "Texas",
+    "Capital": "Austin",
+    "hidden": {
+      "household_proportion": 42.1190998735338,
+      "tag": {}
+    }
+  },
+  "CT": {
+    "Name": "Connecticut",
+    "Capital": "Hartford",
+    "hidden": {
+      "household_proportion": 5.957149251850274,
+      "tag": {}
+    }
+  },
+  "NH": {
+    "Name": "New Hampshire",
+    "Capital": "Concord",
+    "hidden": {
+      "household_proportion": 2.3121889952672956,
+      "tag": {}
+    }
+  },
+  "RI": {
+    "Name": "Rhode Island",
+    "Capital": "Providence",
+    "hidden": {
+      "household_proportion": 1.7839513952568655,
+      "tag": {}
+    }
+  },
+  "VT": {
+    "Name": "Vermont",
+    "Capital": "Montpelier",
+    "hidden": {
+      "household_proportion": 1.1300646237956375,
+      "tag": {}
+    }
+  },
+  "AL": {
+    "Name": "Alabama",
+    "Capital": "Montgomery",
+    "hidden": {
+      "household_proportion": 8.117709179881878,
+      "tag": {}
+    }
+  },
+  "FL": {
+    "Name": "Florida",
+    "Capital": "Tallahassee",
+    "hidden": {
+      "household_proportion": 33.62137061551232,
+      "tag": {}
+    }
+  },
+  "GA": {
+    "Name": "Georgia",
+    "Capital": "Atlanta",
+    "hidden": {
+      "household_proportion": 16.335426617007315,
+      "tag": {}
+    }
+  },
+  "MS": {
+    "Name": "Mississippi",
+    "Capital": "Jackson",
+    "hidden": {
+      "household_proportion": 4.799605390676268,
+      "tag": {}
+    }
+  },
+  "SC": {
+    "Name": "South Carolina",
+    "Capital": "Columbia",
+    "hidden": {
+      "household_proportion": 8.352254010195523,
+      "tag": {}
+    }
+  },
+  "IL": {
+    "Name": "Illinois",
+    "Capital": "Springfield",
+    "hidden": {
+      "household_proportion": 21.060899344201026,
+      "tag": {}
+    }
+  },
+  "IN": {
+    "Name": "Indiana",
+    "Capital": "Indianapolis",
+    "hidden": {
+      "household_proportion": 11.170829331467486,
+      "tag": {}
+    }
+  },
+  "KY": {
+    "Name": "Kentucky",
+    "Capital": "Fankfort",
+    "hidden": {
+      "household_proportion": 7.538507003446313,
+      "tag": {}
+    }
+  },
+  "NC": {
+    "Name": "North Carolina",
+    "Capital": "Raleigh",
+    "hidden": {
+      "household_proportion": 17.23365826311055,
+      "tag": {}
+    }
+  },
+  "OH": {
+    "Name": "Ohio",
+    "Capital": "Columbus",
+    "hidden": {
+      "household_proportion": 20.323066827175893,
+      "tag": {}
+    }
+  },
+  "TN": {
+    "Name": "Tennessee",
+    "Capital": "Nashvile",
+    "hidden": {
+      "household_proportion": 11.287617176804968,
+      "tag": {}
+    }
+  },
+  "VA": {
+    "Name": "Virgina",
+    "Capital": "Richmond",
+    "hidden": {
+      "household_proportion": 13.6941821,
+      "tag": {}
+    }
+  },
+  "WI": {
+    "Name": "Wisconsin",
+    "Capital": "Madison",
+    "hidden": {
+      "household_proportion": 10.248351810726595,
+      "tag": {}
+    }
+  },
+  "WV": {
+    "Name": "West Virgina",
+    "Capital": "Charleston",
+    "hidden": {
+      "household_proportion": 3.1837540905949995,
+      "tag": {}
+    }
+  },
+  "DE": {
+    "Name": "Delaware",
+    "Capital": "Dover",
+    "hidden": {
+      "household_proportion": 1.5789674968818042,
+      "tag": {}
+    }
+  },
+  "MD": {
+    "Name": "Maryland",
+    "Capital": "Annapolis",
+    "hidden": {
+      "household_proportion": 9.583635012451055,
+      "tag": {}
+    }
+  },
+  "NJ": {
+    "Name": "New Jersey",
+    "Capital": "Trenton",
+    "hidden": {
+      "household_proportion": 14.045458298747072,
+      "tag": {}
+    }
+  },
+  "NY": {
+    "Name": "New York",
+    "Capital": "Albany",
+    "hidden": {
+      "household_proportion": 31.913090338590447,
+      "tag": {}
+    }
+  },
+  "PA": {
+    "Name": "Pennsylvania",
+    "Capital": "Harrisburg",
+    "hidden": {
+      "household_proportion": 21.960382614590984,
+      "tag": {}
+    }
+  },
+  "ME": {
+    "Name": "Maine",
+    "Capital": "Augusta",
+    "hidden": {
+      "household_proportion": 2.4333705633613065,
+      "tag": {}
+    }
+  },
+  "MI": {
+    "Name": "Michigan",
+    "Capital": "Lancing",
+    "hidden": {
+      "household_proportion": 17.10136418355418,
+      "tag": {}
+    }
+  },
+  "AK": {
+    "Name": "Alaska",
+    "Capital": "Juneau",
+    "hidden": {
+      "household_proportion": 1.101020856058861,
+      "tag": {}
+    }
+  }
+}
+
+let meta = {
+  "real_household": 230101,
+  "simulated_household": 300,
+  "vacation_state": states["HI"],
+  "vacation_type": {
+    "contact": 5, // How many people do people contact when in vacation?
+  },
+  "money": 0,
 };
+
+let fObject = new Intl.NumberFormat('en-US');
+
+// Sync data
+for (state of Object.keys(states)) {
+  state = states[state];
+  state.Population = fObject.format(Math.round(meta.real_household * state.hidden.household_proportion)) + " People";
+}
+
+
+meta.real_per_simulated = meta.real_household / meta.simulated_household
